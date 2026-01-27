85aa9f6035eaebbbeb344ee50bd6b0b4c888f1b6
kealan-doherty <kealandoherty183@gmail.com>
2026-01-26 22:00:12 -0500

    wrote basic connectToDB function current do not have access to AWS to start creating DB will start that tomorrow once i learn more about packe importing in Go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "github/gin-gonic/gin"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+func main() {
+	// Create the Gin Router
+	router := gin.Default()
 
-func main(){
-  // Create the Gin Router  
-  router := gin.Default() 
-  
-  //Register Routes
-  router.GET("/", HomePage)
+	//Register Routes
+	router.GET("/", HomePage)
 
-  //start the API
-  router.Run()
+	//start the API
+	router.Run()
+	fmt.Printf("server is up and running")
+
 }
-
 
 func HomePage(c *gin.Context) {
-  c.String(http.StatusOK, "this is the home page of the ARC Raiders API router")
+	c.String(http.StatusOK, "this is the home page of the ARC Raiders API router")
 }
-
-
